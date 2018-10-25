f9f60d5cee4a511f37b3d2acd60bc3aa9b1280a9
Trakansak <covon_1@hotmail.com>
2018-10-25 15:38:48 +0700

    added webservice send object and confirm

--- client.php
+++ client.php
@@ -1,13 +1,18 @@
 <?php
 require_once __DIR__ . '/vendor/autoload.php';
-$client = new Zend\Soap\Client('http://127.0.0.1/select1_aircon/server.php?wsdl');
+// $client = new Zend\Soap\Client('http://127.0.0.1/select1_aircon/server.php?wsdl');
 // $client = new Zend\Soap\Client('http://data-aircon.herokuapp.com/server.php?wsdl');
+$client = new Zend\Soap\Client('http://127.0.0.1/select1_aircon/server2.php?wsdl');
 
-    // $result = $client->post_aircon([
-    //     'data_packet' => '81-623,27,8:00 AM'
+    $result = $client->post_aircon([
+        'data_packet' => 'bung,67431,pen,true'
+    ]);
+
+    // $result = $client->info_sendobject([
+    //     'data_object' => 'bung,674/31,pen,true'
     // ]);
 
-    $result = $client->showperson();
-    echo $result->showpersonResult;
+    // echo $result->info_send_objectResult;
+    echo $result->post_airconResult;
     // echo $result;
 ?>
--- server.php
+++ server.php
@@ -90,6 +90,57 @@ class AirCon
         $xml = simplexml_load_file("http://127.0.0.1/select1_aircon/showperson.xml");
         return $xml->asXML();
     }
+
+
+    //service send_object
+
+    public function info_sendobject($data_object)
+    {
+      list($user, $address, $object, $confirm) = explode(",", $data_object);
+        $servername = "ol5tz0yvwp930510.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+        $username = "uohjzu4b27mw2xm6";
+        $password = "rtyq4e1iclt8vtfi";
+        $database = "rjodltazhge2pg94";
+        $conn = new mysqli($servername, $username, $password, $database);
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+            return "Connection failed: " . $conn->connect_error;
+        }
+        echo "Connected successfully";
+        $sql = "INSERT INTO info_send_object (user, addresss, objects, confirm) VALUES (?, ?, ?, ?)";
+        if ($stmt = $conn->prepare($sql)) {
+            $stmt->bind_param('sss', $user, $address, $object, $confirm);
+            $stmt->execute();
+            $stmt->close();
+            return "New record created Successfully";
+        } else {
+            return "Error: " . $sql . "<br>" . $conn->error;
+        }
+    }
+
+    // public function confirm_object($objects)
+    // {
+    //   $servername = "ol5tz0yvwp930510.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+    //   $username = "uohjzu4b27mw2xm6";
+    //   $password = "rtyq4e1iclt8vtfi";
+    //   $database = "rjodltazhge2pg94";
+    //   $conn = new mysqli($servername, $username, $password, $database);
+    //   if ($conn->connect_error) {
+    //       die("Connection failed: " . $conn->connect_error);
+    //       return "Connection failed: " . $conn->connect_error;
+    //   }
+    //   echo "Connected successfully";
+    //   $sql = "SELECT * FROM info_send_object Where Objects ="$object", confirm ='true'"
+    //   $res = mysql_query($sql);
+    //   if ($result->num_rows > 0) {
+    //     if ($stmt = $conn->prepare($sql)) {
+    //       $stmt->bind_param($object);
+    //     return "Object send Complete"
+    //   }
+    //   else{
+    //     return "Object send Not Complete"
+    //   }
+    // }
 }
 
 $serverUrl = "http://127.0.0.1/select1_aircon/server.php";
